1bf565562c3bca9570c01f397c2c352f8bd230fe
AZ174 <zcswzz2004@163.com>
2024-11-22 17:10:15 +0800

    孩子们，这是我们的第一个push

---
+++ flask/flask/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/powerGrid')
+def powerGrid():
+    return render_template("powerGrid.html")
+
+@app.route('/universe')
+def universe():
+    return render_template("universe.html")
+
+@app.route('/superpowerAbilities')
+def superpowerAbilities():
+    return render_template("superpowerAbilities.html")
+
+if __name__ == '__main__':
+    app.run()
